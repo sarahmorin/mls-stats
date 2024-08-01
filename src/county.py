45b08a20a1513577abd2f6c84eb83958ce2b9025
Sarah Morin <smorin8674@gmail.com>
2024-08-01 15:23:59 -0700

    added description

--- src/county.py
+++ src/county.py
@@ -10,11 +10,13 @@ import pandas as pd
 COUNTY_FILE="county_key.csv"
 
 try:
-
     county_df = pd.read_csv(COUNTY_FILE)
 
-# Display Data as Table
+    # Display Data as Table
     st.title("County Key")
+    st.write("""This is the county key used to lookup the county for a given city when entires are
+             uploaded to the database. To add a (city, county) pair scroll to the bottom and enter
+             the data in a new row. When you are done making changes, make sure to click save.""")
 
     county_df = st.data_editor(county_df, num_rows="dynamic")
 
