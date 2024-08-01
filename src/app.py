8d0e872ef53fb72065d828657cb8954a4cd591af
Sarah Morin <smorin8674@gmail.com>
2024-08-01 15:22:44 -0700

    removed unnecessary pages

--- src/app.py
+++ src/app.py
@@ -17,8 +17,6 @@ circle_page = st.Page("circle.py", title="Pie Chart: Sales by Price")
 high_end_page = st.Page("he_sale.py", title="Q vs. Q Sale Distribution")
 sale_over_list_page = st.Page("sol.py", title="Bar: Q vs. Q % Sales over List")
 line_page = st.Page("lines.py", title="Line Graphs: Change in X year over year")
-sale_vs_closed_page = st.Page("sale_v_close.py", title="Line and Bar: Sale Price vs. # Sales")
-med_vs_aom_page = st.Page("med_v_aom.py", title="Line: Median Price vs. AOM")
 
 # ===============================================
 
@@ -32,7 +30,7 @@ pg = st.navigation(
         "Database": [upload_page, browse_page, county_page],
         "Tables": [table_page, q_table_page],
         "Distributions": [sale_dist_page, circle_page, high_end_page],
-        "Graphs": [sale_over_list_page, line_page, sale_vs_closed_page, med_vs_aom_page],
+        "Graphs": [sale_over_list_page, line_page],
     }
 )
 # ===============================================
