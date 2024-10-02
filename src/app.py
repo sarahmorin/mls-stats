893fcac3d720ff79706bdc7195d9818b2d21fcd8
Sarah Morin <smorin8674@gmail.com>
2024-10-02 11:34:34 -0700

    dropped pie chart

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ county_page = st.Page("county.py", title="County Key")
 table_page = st.Page("table.py", title="Sales Stats Table")
 q_table_page = st.Page("qtable.py", title="Q vs. Q Table")
 sale_dist_page = st.Page("dist.py", title="Scatter Line: Price Distribution")
-circle_page = st.Page("circle.py", title="Pie Chart: Sales by Price")
+# circle_page = st.Page("circle.py", title="Pie Chart: Sales by Price")
 high_end_page = st.Page("he_sale.py", title="Q vs. Q Sale Distribution")
 sale_over_list_page = st.Page("sol.py", title="Bar: Q vs. Q % Sales over List")
 line_page = st.Page("lines.py", title="Line Graph: X over time")
@@ -30,7 +30,7 @@ pg = st.navigation(
         "": [home_page],
         "Tables": [table_page, q_table_page],
         "Graphs": [sale_over_list_page, line_page, line_year_page],
-        "Distributions": [sale_dist_page, high_end_page, circle_page],
+        "Distributions": [sale_dist_page, high_end_page],
         "Database": [upload_page, browse_page, county_page],
     }
 )
