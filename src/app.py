5cf8ae6c4d8e5a2a31e75b3f304cbea46b422155
Sarah Morin <smorin8674@gmail.com>
2024-10-04 12:00:10 -0700

    page renames

--- src/app.py
+++ src/app.py
@@ -8,17 +8,17 @@ home_page = st.Page("home.py", title="Home")
 # Data interface
 upload_page = st.Page("upload.py", title="Upload Data")
 browse_page = st.Page("browse.py", title="Browse Database")
-county_page = st.Page("county.py", title="County Key")
+county_page = st.Page("county.py", title="Edit County Key")
 # Graphics
 table_page = st.Page("table.py", title="Sales Stats Table")
-q_table_page = st.Page("qtable.py", title="Q vs. Q Table")
-sale_dist_page = st.Page("dist.py", title="Scatter Line: Price Distribution")
+q_table_page = st.Page("q_table.py", title="Q vs. Q Stats Table")
+sale_dist_page = st.Page("price_dist_line.py", title="Scatter Line: Price Distribution")
 # circle_page = st.Page("circle.py", title="Pie Chart: Sales by Price")
-high_end_page = st.Page("he_sale.py", title="Q vs. Q Sale Distribution")
-sale_over_list_page = st.Page("sol.py", title="Bar: Q vs. Q % Sales over List")
-line_page = st.Page("lines.py", title="Line Graph: X over time")
-line_year_page = st.Page("lines_year_comp.py", title="Line Graphs: Change in X year over year")
-line_sf_page = st.Page("lines_year_sf.py", title="Line Graphs: Change in X year over year SF District")
+q_price_dist_page = st.Page("q_price_dist.py", title="Q vs. Q Price Distribution")
+q_sale_over_list_page = st.Page("q_sale_over_list.py", title="Q vs. Q % Sales over List Price")
+line_page = st.Page("lines.py", title="Stats over time, Compare Areas")
+line_year_page = st.Page("lines_year_county.py", title="Compare 2 Year in County")
+line_year_sf_page = st.Page("lines_year_sf.py", title="Compare 2 Years in SF District")
 
 # ===============================================
 
@@ -29,9 +29,10 @@ st.set_page_config(page_title="MLS DB")
 pg = st.navigation(
     {
         "": [home_page],
-        "Tables": [table_page, q_table_page],
-        "Graphs": [sale_over_list_page, line_page, line_year_page, line_sf_page],
-        "Distributions": [sale_dist_page, high_end_page],
+        "Tables": [table_page],
+        "Distributions": [sale_dist_page],
+        "Line Graphs": [line_page, line_year_page, line_year_sf_page],
+        "Q v. Q Comparisons": [q_table_page, q_price_dist_page, q_sale_over_list_page],
         "Database": [upload_page, browse_page, county_page],
     }
 )
